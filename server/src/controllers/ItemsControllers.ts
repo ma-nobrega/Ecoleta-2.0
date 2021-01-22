d6e6f878ac9bd861c954723943660d6c87a7c382
Matheus Nobrega <matheus.adao.nobrega@gmail.com>
2021-01-22 19:51:20 -0300

    Projeto base rocketseat

---
+++ server/src/controllers/ItemsControllers.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express';
+import knex from '../database/connection'
+class ItemsController {
+  async index(request: Request, response: Response) {
+    const items = await knex('items').select('*');
+    const serializedItems = items.map(items => {
+      return {
+        id: items.id,
+        title: items.title,
+        // image_url: `http://192.168.0.103:3333/uploads/${items.image}`,
+         image_url: `http://localhost:3333/uploads/${items.image}`,
+      };
+    });
+    return response.json(serializedItems);
+  }
+}
+
+export default ItemsController
